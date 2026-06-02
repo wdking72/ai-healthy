62e5a610cb5bf083d5af6c51804cb0bd51a95d13
wdking <wdking7272.outlook.com>
2026-06-02 15:12:20 +0800

    home-header

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/home/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <h1>HOME</h1>
+    </>
+  );
+}
---
+++ src/components/home/header.tsx
@@ -0,0 +1,46 @@
+'use client'
+import Image from "next/image";
+import { Button } from "antd";
+import { useState } from "react";
+
+export default function Header() {
+  // 处理登录状态
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogout = () => {
+    setIsLoggedIn(!isLoggedIn);
+  }
+
+  return (
+    <>
+      <div className="h-[70px] flex items-center justify-between px-40" >
+        <div className="flex items-center gap-2">
+          <Image src="/images/机器人.png" alt="心理健康AI助手" width={50} height={50} /> 
+          <span className="text-2xl font-semibold">心理健康AI助手</span>
+        </div>
+        <div>
+          <nav>
+          <Button className="mx-2 text-lg" type="text">首页</Button>
+          {isLoggedIn ? 
+          <>
+            <Button className="mx-2 text-lg" type="text">AI咨询</Button>
+            <Button className="mx-2 text-lg" type="text">情绪日记</Button>
+          </>
+          : null}
+          <Button className="mx-2 text-lg" type="text">知识库</Button>
+          {isLoggedIn ? 
+          <>
+            <Button className="mx-2 text-lg" onClick={handleLogout}>退出登录</Button>
+          </>
+          : 
+          <>
+            <Button className="mx-2 text-lg" onClick={handleLogout}>登录</Button>
+            <Button className="mx-2 text-lg" type="primary">注册</Button>
+          </>
+          }
+          
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
